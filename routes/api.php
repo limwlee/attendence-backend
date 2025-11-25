ede85a8cec8e308310c8a72d6b6d719cf98f6c74
limwlee <lim14231@gmail.com>
2025-11-26 01:14:58 +0800

    Add debuging api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\Attendance;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,4 +16,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/clock-in', [\App\Http\Controllers\AttendanceController::class, 'clockIn']);
     Route::post('/clock-out', [\App\Http\Controllers\AttendanceController::class, 'clockOut']);
     Route::get('/history', [\App\Http\Controllers\AttendanceController::class, 'history']);
+});
+
+Route::get('/debug/users', function () {
+    return User::all();
+});
+
+Route::get('/debug/attendances', function () {
+    return Attendance::all();
+});
+
+Route::get('/debug/clear-attendances', function () {
+    Attendance::query()->delete();
+    return 'All attendances cleared.';
 });
