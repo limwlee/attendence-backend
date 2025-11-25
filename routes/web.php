3aad12ba9619f526fde0c0debbd870ca1fc811d1
limwlee <lim14231@gmail.com>
2025-11-25 23:28:59 +0800

    Add UserSeeder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/init-db', function () {
+    Artisan::call('migrate', [
+        '--force' => true,
+    ]);
+
+    Artisan::call('db:seed', [
+        '--force' => true,
+    ]);
+
+    return 'migrated & seeded';
 });
